6853127908a8ce4e636263dae6a4938f3cf26c57
Rohan Bari <rohanbari4@gmail.com>
2020-12-31 01:08:25 +0530

    Enhancement in HelloSwing class

--- src/com/rohanbari/jswing/HelloSwing.java
+++ src/com/rohanbari/jswing/HelloSwing.java
@@ -5,11 +5,28 @@ import javax.swing.*;
 public class HelloSwing extends JFrame {
 
     public HelloSwing() {
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Click Me");
+
+        // Most fundamental UI configuration
         this.setTitle("Welcome to JSwing");
         this.setSize(250, 250);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setVisible(true);
+
+        // Adding ActionListener event to JButton
+        button.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null,
+                    "You can write a better code here to contribute!",
+                    "First Step Taken!",
+                    JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0); // Closes after displaying the message
+        });
+
+        panel.add(button);
+        this.add(panel);
     }
 
     public static void main(String[] args) {
